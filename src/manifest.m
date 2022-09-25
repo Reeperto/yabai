001817d2d93015310d53e0220c9ec47b6be59b9a
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-09-25 02:30:17 +0200

    upgrade IPC from unix sockets to mach messages

--- src/manifest.m
+++ src/manifest.m
@@ -37,6 +37,7 @@
 //#include "misc/autorelease.h"
 #include "misc/notify.h"
 #include "misc/log.h"
+#include "misc/mach.h"
 #include "misc/helpers.h"
 #include "misc/sbuffer.h"
 #define HASHTABLE_IMPLEMENTATION
@@ -65,6 +66,7 @@
 #include "window_manager.h"
 #include "mouse.h"
 
+#include "misc/mach.c"
 #include "sa.m"
 #include "mission_control.c"
 #include "event_loop.c"
